c4f6dc3fd53b744a2833d1b367b1a3dc6f4a791d
Shivam1 Srivastava <shivam1.srivastava@laitkor.com>
2014-05-28 19:12:56 +0530

    Added Converter Class for image

---
+++ inventory/Converters/ImageToSourceConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace inventory.Converters
+{
+    public class ImageToSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType == typeof(ImageSource))
+            {
+                if (value is string)
+                {
+                    string str = (string)value;
+                    return new BitmapImage(new Uri(str, UriKind.RelativeOrAbsolute));
+                }
+                else if (value is Uri)
+                {
+                    Uri uri = (Uri)value;
+                    return new BitmapImage(uri);
+                }
+            }
+            return value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
